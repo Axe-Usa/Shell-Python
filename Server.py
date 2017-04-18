1e8a7c24af9e28f496cb39ee5ad07addf8c92633
Axe-Usa <naimatropical@gmail.com>
2017-04-18 14:29:52 +0200

    Server

---
+++ Server.py
@@ -0,0 +1,27 @@
+import socket
+import threading
+
+ 
+def administrar_clientes(socket_cliente):
+    peticion = socket_cliente.recv(1024)
+    print "[*] Mensaje recibido: %s" % peticion
+    #respuesta = Popen(peticion, shell=True, stdout=PIPE).stdout.read()
+    # confirmar al cliente, que el mensaje fue recibido
+    #socket_cliente.send(respuesta)
+    #socket_cliente.close()
+ 
+ip = "0.0.0.0"
+puerto = 39421
+max_conexiones = 5
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+servidor.bind((ip, puerto))
+servidor.listen(max_conexiones)
+ 
+print "[*] Esperando conexiones en %s:%d" % (ip, puerto)
+ 
+while True:
+    cliente, direccion = servidor.accept()
+    print "[*] Conexion establecida con %s:%d" % (direccion[0], direccion[1])
+    administrador_de_clientes = threading.Thread(target=administrar_clientes, args=(cliente,))
+    administrador_de_clientes.start()
